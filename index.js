5aa05376141281fac67aa596ac556f8f1c5a6de1
Isa Citra Buana <110759969+isaui@users.noreply.github.com>
2023-08-29 16:37:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,28 @@ import passport from 'passport';
 import LocalStrategy from 'passport-local';
 import Comment from './models/Comment.js';
 
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
+
 
 
 
@@ -50,6 +72,7 @@ const resetAll = async () => {
   await User.deleteMany({})
 }
 app.use(cors());
+app.use(allowCors(handler));
 app.use(express.json());
 app.use(session(sessionConfig));
 app.use(flash());
