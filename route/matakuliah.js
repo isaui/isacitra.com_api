ffed24986c22a60f9ace195f989f3b4cba865c5c
isaui <isadestroyed17@gmail.com>
2023-09-15 15:04:33 +0700

    yyyyy

--- route/matakuliah.js
+++ route/matakuliah.js
@@ -58,6 +58,34 @@ router.post('/edit/:id', asyncWrapper(async (req,res)=>{
   }
 }))
 
+// Menambahkan Notes
+router.post('/addNotes/:id', asyncWrapper(async (req,res)=>{
+  const {id} = req.params;
+  const {idMatkul, idChapter, dataMateri } = req.body;
+  try {
+      const matkul = await  MataKuliah.findById(id).populate('author').populate('chapters').populate({
+        path: 'chapters.materi.notes.author',
+        model: 'User' // Ganti 'User' dengan nama model yang sesuai
+    }).populate({
+      path: 'chapters.materi.videos.author',
+      model: 'User' // Ganti 'User' dengan nama model yang sesuai
+  });
+
+  if(!matkul){
+      return res.status(400).json({message:"Mata Kuliah Tidak Ada atau Telah Dihapus"})
+  }
+  const chapter = matkul.chapters.find((ch) => ch._id == idChapter);
+  if(chapter){
+    chapter.notes.push(dataMateri)
+  }
+  await matkul.save();
+  channel.publish('update-matkul', matkul)
+  return res.json(matkul);
+  } catch (error) {
+      return res.status(500).json({error:error, message:error.message??"Terjadi kesalahan dalam server"})
+  }
+}))
+
 
 router.get('/', async (req, res) => {
     try {
