4fde41e50a0609dc6977452d0ad75015ab2588f8
isaui <isadestroyed17@gmail.com>
2024-01-17 07:47:15 +0700

    hehey

--- route/db.js
+++ route/db.js
@@ -1,7 +1,194 @@
 import express from 'express';
 import query from '../db/query.js';
 import asyncWrapper from '../utils/async-wrapper.js';
+import { formatDate, formatISO8601ToHHMM } from '../utils/modules/demo.js';
+import nodemailer from 'nodemailer';
 const router = express.Router();
+
+const sendNotificationEmail = async (id) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail', // misalnya 'gmail'
+    port: 587,
+    host: 'smtp.gmail.com',
+    secure: false,
+    auth: {
+        user: 'isacitralearning@gmail.com',
+        pass: 'rtsj nhtr lwya pzcb'
+    }
+  });
+  const queryResult = await query(`SELECT BOOKING_DEMO.email, BOOKING_DEMO.nomorMahasiswa, BOOKING_DEMO.platform, BOOKING_DEMO.bookingDemoId,
+          DEMO_SESSION.startTime, DEMO_SESSION.endTime, DEMO_SESSIONS_DATE.date,
+          DEMO_EVENT.title, DEMO_EVENT.startDate, DEMO_EVENT.endDate FROM BOOKING_DEMO INNER JOIN DEMO_SESSION ON BOOKING_DEMO.demoSessionId = DEMO_SESSION.demoSessionId 
+          INNER JOIN DEMO_SESSIONS_DATE ON DEMO_SESSIONS_DATE.demoSessionDateId = DEMO_SESSION.demoSessionDateId INNER JOIN DEMO_EVENT ON
+          DEMO_SESSIONS_DATE.demoEventId = DEMO_SESSIONS_DATE.demoEventId
+          WHERE BOOKING_DEMO.bookingDemoId = '${id}' LIMIT 1;`)
+          const data = queryResult.rows[0];
+          const recipientEmail = data.email;
+          const npm = data.nomormahasiswa;
+          const platform = data.platform;
+          const startTime = data.starttime;
+          const endTime = data.endtime;
+          const eventName = data.title;
+          const date = data.date
+// EMAIL DATA -----------------------------------------------------------
+          const subject = `Mendaftar ${eventName}`
+          const html = `
+          <!doctype html>
+          <html>
+          <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          </head>
+          <body>
+            <h1 style="margin-bottom: 0.5rem; 
+            font-size: 1.25rem;
+            line-height: 1.75rem; 
+            font-weight: 700; 
+            color: #4F46E5; 
+            ">
+              Permisi, ${npm}
+            </h1>
+            <p style="margin-bottom: 0.5rem; 
+            font-size: 1rem;
+            line-height: 1.5rem; 
+            text-align: justify;
+            ">
+            Sebelumnya, terimakasih telah mendaftar pada ${eventName}. Anda telah memilih untuk melakukan
+            demo tersebut pada waktu ${formatISO8601ToHHMM(startTime)} - ${formatISO8601ToHHMM(endTime)} WIB pada tanggal ${formatDate(date)}.
+            Apabila Anda berencana untuk mengubah jadwal Anda, silahkan kunjungi web dibawah:
+            </p>
+            <div  style='display: flex; margin-bottom: 0.5rem; flex-direction: column; 
+            justify-content: center; align-items: center;  border-radius: 0.375rem;'>
+             <a href="${'https://isacitra.com/jadwal'}" style="padding-top: 0.25rem; padding-bottom: 0.25rem; padding-left: 0.5rem;
+            padding-right: 0.5rem; border-radius: 0.375rem; font-size: 1.125rem;
+             line-height: 1.75rem; 
+             text-align: center; 
+             color: #ffffff; 
+             background-color: #3730A3;
+             ">MELUNCUR</a>
+            </div>
+            <p style="margin-bottom: 0.5rem; 
+            font-size: 1rem;
+            line-height: 1.5rem; 
+            text-align: justify;
+            ">
+            Pada demo tersebut, Anda memilih menggunakan platform ${platform}. 
+            ${platform == 'discord'? 'Seperti biasa di channel discord, Ok.' : 'Nanti saya akan infokan linknya.'}
+            </p>
+            <p style="margin-bottom: 0.5rem; 
+            font-size: 1rem;
+            line-height: 1.5rem; 
+            text-align: justify;
+            ">
+            Isa.
+            </p>
+          </body>
+          </html>
+          
+          `
+// EMAIL OPTIONS ------------------------------------------------------------
+          const mailOptions = {
+            from: {
+              name: 'Isa Citra',
+              address:'isacitralearning@gmail.com'
+            },
+            to: recipientEmail,
+            subject: subject,
+            html: html
+        };
+        
+        // Kirim email --------------------------------------------------------
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
+
+}
+
+const sendInformationEmail = async (id, text, subject) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail', // misalnya 'gmail'
+    port: 587,
+    host: 'smtp.gmail.com',
+    secure: false,
+    auth: {
+        user: 'isacitralearning@gmail.com',
+        pass: 'rtsj nhtr lwya pzcb'
+    }
+  });
+  const queryResult = await query(`SELECT BOOKING_DEMO.email, BOOKING_DEMO.nomorMahasiswa, BOOKING_DEMO.platform, BOOKING_DEMO.bookingDemoId,
+          DEMO_SESSION.startTime, DEMO_SESSION.endTime, DEMO_SESSIONS_DATE.date,
+          DEMO_EVENT.title, DEMO_EVENT.startDate, DEMO_EVENT.endDate FROM BOOKING_DEMO INNER JOIN DEMO_SESSION ON BOOKING_DEMO.demoSessionId = DEMO_SESSION.demoSessionId 
+          INNER JOIN DEMO_SESSIONS_DATE ON DEMO_SESSIONS_DATE.demoSessionDateId = DEMO_SESSION.demoSessionDateId INNER JOIN DEMO_EVENT ON
+          DEMO_SESSIONS_DATE.demoEventId = DEMO_SESSIONS_DATE.demoEventId
+          WHERE BOOKING_DEMO.bookingDemoId = '${id}' LIMIT 1;`)
+          const data = queryResult.rows[0];
+          const recipientEmail = data.email;
+          const npm = data.nomormahasiswa;
+          const platform = data.platform;
+          const startTime = data.starttime;
+          const endTime = data.endtime;
+          const eventName = data.title;
+          const date = data.date
+  
+// EMAIL DATA -----------------------------------------------------------
+  const html = `
+          <!doctype html>
+          <html>
+          <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          </head>
+          <body>
+            <h1 style="margin-bottom: 0.5rem; 
+            font-size: 1.25rem;
+            line-height: 1.75rem; 
+            font-weight: 700; 
+            color: #4F46E5; 
+            ">
+              Update Demo
+            </h1>
+            <p style="margin-bottom: 0.5rem; 
+            font-size: 1rem;
+            line-height: 1.5rem; 
+            text-align: justify;
+            ">Permisi ${npm}, 
+            ${eventName} dengan id ${id} yang dilaksanakan pada ${formatISO8601ToHHMM(startTime)} - ${formatISO8601ToHHMM(endTime)} WIB ${text}
+            </p>
+            <p style="margin-bottom: 0.5rem; 
+            font-size: 1rem;
+            line-height: 1.5rem; 
+            text-align: justify;
+            ">
+            Isa.
+            </p>
+          </body>
+          </html>
+          
+          `
+    // EMAIL OPTIONS ------------------------------------------------------------
+    const mailOptions = {
+      from: {
+        name: 'Isa Citra',
+        address:'isacitralearning@gmail.com'
+      },
+      to: recipientEmail,
+      subject: subject,
+      html: html
+  };
+  
+  // Kirim email --------------------------------------------------------
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          console.error(error);
+      } else {
+          console.log('Email sent: ' + info.response);
+      }
+  });
+}
 
 router.post('/', asyncWrapper(async (req, res) => {
     try {
@@ -9,6 +196,24 @@ router.post('/', asyncWrapper(async (req, res) => {
         const {queryString} = req.body
         const queryResult = await query(queryString)
         res.json({ message: 'Berhasil mengquery', result: queryResult });
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).json({ message: error.toString() });
+    }
+  }));
+  router.post('/schedule-email', asyncWrapper(async (req, res) => {
+
+   
+    try {
+
+        const {id, taskType, text, subject} = req.body
+        if(taskType == 'NOTIFY'){
+          await sendNotificationEmail(id)
+        }
+        else if(taskType == 'INFO'){
+          await sendInformationEmail(id, text, subject)
+        }
+        res.json({ message: 'Berhasil mengquery'});
     } catch (error) {
       console.error('Error:', error);
       res.status(500).json({ message: error.toString() });
---
+++ utils/modules/demo.js
@@ -0,0 +1,92 @@
+function formatISO8601ToHHMM(iso8601DateTime) {
+    const isoStringWithUTC = iso8601DateTime;
+
+    const dateObj = new Date(isoStringWithUTC);
+    dateObj.setUTCHours(dateObj.getUTCHours() + 14);
+    const hours = String(dateObj.getUTCHours()).padStart(2, '0');
+    const minutes = String(dateObj.getUTCMinutes()).padStart(2, '0');
+    const formattedTime = `${hours}:${minutes}`;
+  
+    return formattedTime;
+  }
+const compareDates = (date1, date2) => {
+    const year1 = date1.getFullYear();
+    const month1 = date1.getMonth();
+    const day1 = date1.getDate();
+  
+    const year2 = date2.getFullYear();
+    const month2 = date2.getMonth();
+    const day2 = date2.getDate();
+  
+    return year1 === year2 && month1 === month2 && day1 === day2;
+  };
+
+const formatDate = (inputDate) => {
+    // Buat objek Date dari string tanggal
+    const dateObject = new Date(inputDate);
+  
+    // Daftar nama bulan dalam bahasa Inggris
+    const monthNames = [
+      'Januari', 'Februari', 'Maret',
+      'April', 'Mei', 'Juni', 'Juli',
+      'Agustus', 'September', 'Oktober',
+      'November', 'Desember'
+    ];
+  
+    // Ambil informasi tanggal, bulan, dan tahun dari objek Date
+    const day = dateObject.getDate();
+    const monthIndex = dateObject.getMonth();
+    const year = dateObject.getFullYear();
+  
+    // Konstruksi string dengan format yang diinginkan
+    const formattedDate = `${day} ${monthNames[monthIndex]} ${year}`;
+  
+    return formattedDate;
+  };
+
+  function toTimestamp(timeStr, dateStr) {
+    // Dapatkan string datetime dalam format ISO 8601
+    const iso8601DateTime = `${dateStr}T${timeStr}:00Z`;
+  
+    // Buat objek Date dari string iso8601DateTime
+
+  
+    return iso8601DateTime;
+  }
+
+  const formatDateToString = (dateObject) => {
+    const monthNames = [
+        'Januari', 'Februari', 'Maret',
+        'April', 'Mei', 'Juni', 'Juli',
+        'Agustus', 'September', 'Oktober',
+        'November', 'Desember'
+      ];
+    
+      // Ambil informasi tanggal, bulan, dan tahun dari objek Date
+      const day = dateObject.getDate();
+      const monthIndex = dateObject.getMonth();
+      const year = dateObject.getFullYear();
+    
+      // Konstruksi string dengan format yang diinginkan
+      const formattedDate = `${day} ${monthNames[monthIndex]} ${year}`;
+    
+      return formattedDate;
+  }
+
+  function areDateStringEqual(dateString1, dateString2) {
+    // Buat objek Date dari string tanggal pertama
+    const date1 = new Date(dateString1);
+    // Buat objek Date dari string tanggal kedua
+    const date2 = new Date(dateString2);
+  
+    // Bandingkan kedua tanggal tanpa memperhatikan jam, menit, detik, dan milidetik
+    const strippedDate1 = new Date(date1.toDateString());
+    const strippedDate2 = new Date(date2.toDateString());
+  
+    return strippedDate1.getTime() === strippedDate2.getTime();
+  }
+
+  export{
+    formatDate,
+    formatISO8601ToHHMM
+  }
